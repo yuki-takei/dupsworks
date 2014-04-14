ff399ba8a6d02bca5710402d23b1f2005bf975fe
Yuki Takei <yuki@weseek.co.jp>
2014-04-14 10:02:56 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ import boto, boto.ec2, boto.opsworks
 from boto.vpc import VPCConnection
 from boto.opsworks.layer1 import OpsWorksConnection
 
-ec2region = boto.ec2.get_region("ap-northeast-1")
 
 # create VPCConnection
+ec2region = boto.ec2.get_region("ap-northeast-1")
 vpc_conn = boto.vpc.VPCConnection(region=ec2region)
 # create a VPC
 vpc = vpc_conn.create_vpc('10.20.0.0/16')
@@ -37,39 +37,56 @@ subnet_b_pvt = vpc_conn.create_subnet(vpc.id, "10.20.129.0/24", 'ap-northeast-1c
 
 
 # create OpsWorksConnection
-ow_conn = boto.opsworks.layer1.OpsWorksConnection()
+#ow_conn = boto.opsworks.layer1.OpsWorksConnection()
 # create a Stack
-result = ow_conn.create_stack('Test', 'ap-northeast-1', 'arn:aws:iam::259692501178:role/aws-opsworks-service-role', 'arn:aws:iam::259692501178:instance-profile/aws-opsworks-ec2-role', vpc.id, default_subnet_id=subnet_a_pvt.id)
+#result = ow_conn.create_stack('Test', 'ap-northeast-1', 'arn:aws:iam::259692501178:role/aws-opsworks-service-role', 'arn:aws:iam::259692501178:instance-profile/aws-opsworks-ec2-role', vpc.id, default_subnet_id=subnet_a_pvt.id)
+#stack_id = result.StackId
+
+# TODO create nat instances
+
+# TODO setup route tables
+
+# TODO create and set Custom Json
+
+# TODO start nat instances
 
 
 
+def create_and_replace_route_table(conn, subnet):
+    # create new route table
+    new_rtb = conn.create_route_table(vpc.id)
 
-def setup_vpc(conn):
-    vpc = vpc_conn.create_vpc('10.20.0.0/16')
+    # replace
+    rtb = conn.associate_route_table(new_rtb.id, subnet.id)
 
-    # create the main subnet
-    subnet_a_pub = conn.create_subnet(vpc.id, "10.20.0.0/24", 'ap-northeast-1c')
-    # create an Internet Gateway
+    return new_rtb
+
+
+def setup_route_tables(conn):
+    rtb_a_pub = create_and_replace_route_table(conn, subnet_a_pub)
+    rtb_a_pvt = create_and_replace_route_table(conn, subnet_a_pvt)
+    rtb_b_pub = create_and_replace_route_table(conn, subnet_b_pub)
+    rtb_a_pvt = create_and_replace_route_table(conn, subnet_b_pvt)
+    
+    # set Internet Gateway to public subnet
     igw = conn.create_internet_gateway()
     conn.attach_internet_gateway(igw.id, vpc.id)
+    conn.create_route(rtb_a_pub.id, "0.0.0.0/0", gateway_id=igw.id)
+    conn.create_route(rtb_b_pub.id, "0.0.0.0/0", gateway_id=igw.id)
 
-    # get main route table (should have been created automatically)
-    rtb_main = conn.get_all_route_tables(filters=(("vpc-id", vpc.id),))[0]
-    conn.create_route(rtb_main.id, "0.0.0.0/0", gateway_id=igw.id)
+def remove_orphan_route_tables(conn):
+    # get all route tables
+    all_tables = conn.get_all_route_tables(filters=(("vpc-id", vpc.id),))
 
-    # create other subnets
-    subnet_a_pvt = conn.create_subnet(vpc.id, "10.20.128.0/24", 'ap-northeast-1c')
-#    rtb_a_pvt = conn.create_route_table(vpc.id)
-    subnet_b_pub = conn.create_subnet(vpc.id, "10.20.1.0/24", 'ap-northeast-1c')
-    subnet_b_pvt = conn.create_subnet(vpc.id, "10.20.129.0/24", 'ap-northeast-1c')
+    for rtb in all_tables:
+        if rtb.associations[0].subnet_id == None:
+            conn.delete_route_table(rtb.id)
 
 
-
-#setup_subnets(vpc, vpc_conn)
-
+setup_route_tables(vpc_conn)
+remove_orphan_route_tables(vpc_conn)
 
 print("Created VPC " + vpc.id)
-print(result)
 
 #s3 = boto.opsworks.connect_to_region('us-east-1')
 
