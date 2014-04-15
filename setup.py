f37de98671eac5d9512b846e89b55dc3658e0ee6
Yuki Takei <yuki@weseek.co.jp>
2014-04-15 21:51:25 +0900

    BugFix

--- setup.py
+++ setup.py
@@ -24,23 +24,65 @@ from boto.vpc import VPCConnection
 from boto.opsworks.layer1 import OpsWorksConnection
 
 
+
+
+
+def create_and_associate_route_table(conn, subnet):
+    # create new route table
+    rtb = conn.create_route_table(vpc.id)
+
+    # replace
+    conn.associate_route_table(rtb.id, subnet.id)
+
+    return rtb
+
+
+def setup_internet_gateway(conn, rtbs):
+    # set Internet Gateway to public subnet
+    igw = conn.create_internet_gateway()
+    conn.attach_internet_gateway(igw.id, vpc.id)
+
+    for rtb in rtbs:
+        conn.create_route(rtb.id, "0.0.0.0/0", gateway_id=igw.id)
+        conn.create_route(rtb.id, "0.0.0.0/0", gateway_id=igw.id)
+
+#s3 = boto.opsworks.connect_to_region('us-east-1')
+
+#print boto.opsworks.regions()
+
+
+
+
 # create VPCConnection
 ec2region = boto.ec2.get_region("ap-northeast-1")
+ec2_conn = boto.ec2.connect_to_region("ap-northeast-1")
 vpc_conn = boto.vpc.VPCConnection(region=ec2region)
 # create a VPC
-vpc = vpc_conn.create_vpc('10.20.0.0/16')
-subnet_a_pub = vpc_conn.create_subnet(vpc.id, "10.20.0.0/24", 'ap-northeast-1c')
-subnet_a_pvt = vpc_conn.create_subnet(vpc.id, "10.20.128.0/24", 'ap-northeast-1c')
-subnet_b_pub = vpc_conn.create_subnet(vpc.id, "10.20.1.0/24", 'ap-northeast-1c')
-subnet_b_pvt = vpc_conn.create_subnet(vpc.id, "10.20.129.0/24", 'ap-northeast-1c')
+vpc = vpc_conn.create_vpc('10.24.0.0/16')
+subnet_a_pub = vpc_conn.create_subnet(vpc.id, "10.24.0.0/24", 'ap-northeast-1a')
+rtb_main = vpc_conn.get_all_route_tables(filters=(("vpc-id", vpc.id),))[0]	# get main route table
+subnet_a_pvt = vpc_conn.create_subnet(vpc.id, "10.24.128.0/24", 'ap-northeast-1a')
+subnet_b_pub = vpc_conn.create_subnet(vpc.id, "10.24.1.0/24", 'ap-northeast-1c')
+subnet_b_pvt = vpc_conn.create_subnet(vpc.id, "10.24.129.0/24", 'ap-northeast-1c')
+# get or create route tables
+rtb_a_pub = rtb_main
+vpc_conn.associate_route_table(rtb_a_pub.id, subnet_a_pub.id)			# set main route table
+rtb_a_pvt = create_and_associate_route_table(vpc_conn, subnet_a_pvt)
+rtb_b_pub = create_and_associate_route_table(vpc_conn, subnet_b_pub)
+rtb_b_pvt = create_and_associate_route_table(vpc_conn, subnet_b_pvt)
 
+setup_internet_gateway(vpc_conn, [rtb_a_pub, rtb_b_pub])
+
+print("VPC has been created : " + vpc.id)
 
 
 # create OpsWorksConnection
-#ow_conn = boto.opsworks.layer1.OpsWorksConnection()
+ow_conn = boto.opsworks.layer1.OpsWorksConnection()
 # create a Stack
-#result = ow_conn.create_stack('Test', 'ap-northeast-1', 'arn:aws:iam::259692501178:role/aws-opsworks-service-role', 'arn:aws:iam::259692501178:instance-profile/aws-opsworks-ec2-role', vpc.id, default_subnet_id=subnet_a_pvt.id)
-#stack_id = result.StackId
+result = ow_conn.create_stack('Test', 'ap-northeast-1', 'arn:aws:iam::259692501178:role/aws-opsworks-service-role', 'arn:aws:iam::259692501178:instance-profile/aws-opsworks-ec2-role', vpc.id, default_subnet_id=subnet_a_pvt.id)
+stack_id = result[StackId]
+
+print "OpsWorks Stack has been created : " + stack_id
 
 # TODO create nat instances
 
@@ -50,44 +92,3 @@ subnet_b_pvt = vpc_conn.create_subnet(vpc.id, "10.20.129.0/24", 'ap-northeast-1c
 
 # TODO start nat instances
 
-
-
-def create_and_replace_route_table(conn, subnet):
-    # create new route table
-    new_rtb = conn.create_route_table(vpc.id)
-
-    # replace
-    rtb = conn.associate_route_table(new_rtb.id, subnet.id)
-
-    return new_rtb
-
-
-def setup_route_tables(conn):
-    rtb_a_pub = create_and_replace_route_table(conn, subnet_a_pub)
-    rtb_a_pvt = create_and_replace_route_table(conn, subnet_a_pvt)
-    rtb_b_pub = create_and_replace_route_table(conn, subnet_b_pub)
-    rtb_a_pvt = create_and_replace_route_table(conn, subnet_b_pvt)
-    
-    # set Internet Gateway to public subnet
-    igw = conn.create_internet_gateway()
-    conn.attach_internet_gateway(igw.id, vpc.id)
-    conn.create_route(rtb_a_pub.id, "0.0.0.0/0", gateway_id=igw.id)
-    conn.create_route(rtb_b_pub.id, "0.0.0.0/0", gateway_id=igw.id)
-
-def remove_orphan_route_tables(conn):
-    # get all route tables
-    all_tables = conn.get_all_route_tables(filters=(("vpc-id", vpc.id),))
-
-    for rtb in all_tables:
-        if rtb.associations[0].subnet_id == None:
-            conn.delete_route_table(rtb.id)
-
-
-setup_route_tables(vpc_conn)
-remove_orphan_route_tables(vpc_conn)
-
-print("Created VPC " + vpc.id)
-
-#s3 = boto.opsworks.connect_to_region('us-east-1')
-
-#print boto.opsworks.regions()
