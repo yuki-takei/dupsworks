4dd05cf9d06c05035404cd743244e5290b268ab9
Yuki Takei <yuki@weseek.co.jp>
2014-04-17 06:02:58 +0900

    slimup modules

--- dupsworks/opsworks.py
+++ dupsworks/opsworks.py
@@ -28,69 +28,6 @@ def set_name(resource, name):
     conn.create_tags([resource.id], {"Name": name})
 
 
-def setup_stack():
-    global ctx, cfg, cfg_p, cfg_o
-    global conn
-
-    # create a Stack
-    result = conn.create_stack('Test', 'ap-northeast-1',
-        'arn:aws:iam::259692501178:role/aws-opsworks-service-role',
-        'arn:aws:iam::259692501178:instance-profile/aws-opsworks-ec2-role',
-        ctx.vpc.id, default_subnet_id=ctx.subnet_a_pvt.id,
-        default_root_device_type="ebs",
-        use_custom_cookbooks=True,
-        custom_cookbooks_source={
-            "Type": "git",
-            "Url": "https://github.com/yuki-takei/dupsworks-tmplate-cc.git"
-        })
-    stack_id = result["StackId"]
-    
-    print "OpsWorks Stack has been created : " + stack_id
-    
-    # Chef settings
-    # !! using awscli because boto(at least 2.27.0) doesn't support the "chef-configuration" argument !!
-    subprocess.check_output("aws --region us-east-1 \
-        opsworks update-stack --stack-id %s \
-            --configuration-manager Name=Chef,Version=11.10 \
-            --chef-configuration ManageBerkshelf=true,BerkshelfVersion=2.0.14" % stack_id,
-        shell=True)
-    
-    # create admin layer
-    result = conn.create_layer(stack_id, 'custom', 'Admin Server', 'admin',
-        auto_assign_elastic_ips=True, custom_recipes={"Setup": ["timezone"]})
-    layer_id_admin = result["LayerId"]
-    print("OpsWorks Layer 'Admin Server' has been created : " + layer_id_admin)
-    # create nat layer
-    result = ow_conn.create_layer(stack_id, 'custom', 'NAT Server', 'nat',
-        auto_assign_public_ips=True, custom_recipes={
-            "Setup": [
-                "timezone",
-                "vpcnat::disable-source-dest-check", "vpcnat::setup-heartbeat-script"
-            ]
-        })
-    layer_id_nat = result["LayerId"]
-    print("OpsWorks Layer 'NAT Server' has been created : " + layer_id_nat)
-
-
-    # create an admin instance
-    result = conn.create_instance(stack_id, [layer_id_admin], 't1.micro', subnet_id=ctx.subnet_a_pub.id)
-    instance_opsid_admin = result["InstanceId"]
-    print("1 admin instance has been created : " + instance_opsid_admin)
-    
-    # create nat instances
-    result = conn.create_instance(stack_id, [layer_id_nat], 't1.micro', subnet_id=ctx.subnet_a_pub.id)
-    instance_opsid_nat_az1 = result["InstanceId"]
-    result = conn.create_instance(stack_id, [layer_id_nat], 't1.micro', subnet_id=ctx.subnet_b_pub.id)
-    instance_opsid_nat_az2 = result["InstanceId"]
-    print("2 nat instances has been created : " + instance_opsid_nat_a + ", " + instance_opsid_nat_b)
-    
-    # set to context variable
-    ctx.stack_id = stack_id
-    ctx.instance_opsid_admin = instance_opsid_admin
-    ctx.instance_opsid_nat_az1 = instance_opsid_nat_az1
-    ctx.instance_opsid_nat_az2 = instance_opsid_nat_az2
-
-
 def get_ec2id_from_opsid(opsid):
     global ctx, cfg, cfg_p, cfg_o
     global conn
--- dupsworks/vpc.py
+++ dupsworks/vpc.py
@@ -28,60 +28,6 @@ def init(context):
     ec2region = boto.ec2.get_region(cfg_p["region"])
     conn = boto.vpc.VPCConnection(region=ec2region)
 
-def setup_vpc():
-    global ctx, cfg, cfg_p, cfg_o
-    global conn
-    global subnet_az1_pub, sunet_a_pvt, subnet_az2_pub, subnet_az2_pvt
-    global rtb_az1_pub, rtb_az1_pvt, rtb_az2_pub, rtb_az2_pvt
-
-
-    # create a VPC
-    vpc = conn.create_vpc(cfg_p["vpc_cidr"])
-    dupsworks.ec2.set_name(vpc, cfg_p["vpc_name"])
-
-    # create subnets
-    tpl = cfg["VPC"]["vpc_subnet_name_template"]
-    subnet_az1_pub = create_subnet(cfg_p["vpc_subnet_az1_public_cidr"], cfg_p["vpc_subnet_az1"],
-        tpl % {"vpc_name": cfg_p["vpc_name"], "layer": "public", "group": "A"})
-    rtb_main = conn.get_all_route_tables(filters=(("vpc-id", vpc.id),))[0]      # get main route table
-    subnet_az1_pvt = create_subnet(cfg_p["vpc_subnet_az1_private_cidr"], cfg_p["vpc_subnet_az1"],
-        tpl % {"vpc_name": cfg_p["vpc_name"], "layer": "private", "group": "B"})
-    subnet_az2_pub = create_subnet(cfg_p["vpc_subnet_az2_public_cidr"], cfg_p["vpc_subnet_az2"],
-        tpl % {"vpc_name": cfg_p["vpc_name"], "layer": "public", "group": "A"})
-    subnet_az2_pvt = create_subnet(cfg_p["vpc_subnet_az2_private_cidr"], cfg_p["vpc_subnet_az2"],
-        tpl % {"vpc_name": cfg_p["vpc_name"], "layer": "private", "group": "B"})
-    # get or create route tables
-    rtb_az1_pub = rtb_main
-    tpl = cfg["VPC"]["vpc_rtb_name_template"]
-    conn.associate_route_table(rtb_az1_pub.id, subnet_az1_pub.id)                   # set main route table
-    dupsworks.ec2.set_name(rtb_az1_pub,
-        tpl % {"vpc_name": cfg_p["vpc_name"], "layer": "public", "group": "A"})
-    rtb_az1_pvt = create_and_associate_route_table(subnet_az1_pvt,
-        tpl % {"vpc_name": cfg_p["vpc_name"], "layer": "private", "group": "A"})
-    rtb_az2_pub = create_and_associate_route_table(subnet_az2_pub,
-        tpl % {"vpc_name": cfg_p["vpc_name"], "layer": "public", "group": "B"})
-    rtb_az2_pvt = create_and_associate_route_table(subnet_az2_pvt,
-        tpl % {"vpc_name": cfg_p["vpc_name"], "layer": "private", "group": "B"})
-    # set Internet Gateway
-    tpl = cfg["VPC"]["vpc_igw_name_template"]
-    setup_internet_gateway([rtb_az1_pub, rtb_az2_pub],
-        tpl % {"vpc_name": cfg_p["vpc_name"]})
-
-    print("VPC has been created : " + vpc.id)
-
-    # set to context variable
-    ctx.vpc = vpc
-    ctx.subnet_az1_pub = subnet_az1_pub
-    ctx.subnet_az1_prv = subnet_az1_prv
-    ctx.subnet_az2_pub = subnet_az2_pub
-    ctx.subnet_az2_prv = subnet_az2_priv
-    ctx.rtb_az1_pub = rtb_az1_pub
-    ctx.rtb_az1_pvt = rtb_az1_pvt
-    ctx.rtb_az2_pub = rtb_az2_pub
-    ctx.rtb_az2_pvt = rtb_az2_pvt
-
-
-
 def create_subnet(cidr_block, availability_zone, name=""):
     global conn, vpc
 
